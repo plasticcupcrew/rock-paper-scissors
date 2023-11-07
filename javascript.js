194a4fbf775079b0a3387aa998fa70b71ec2cb7f
Michael Borrello <mborrello1@gmail.com>
2023-11-07 12:25:37 -0500

    Create function that plays a 5 round game
    Loop through the playOneRound function until the total score
    between both users is 5 (increment "i" if there is a win or loss,
    decrement "i" if there is a tie). Print the resulting scores
    and a message with the winner of the game.

--- javascript.js
+++ javascript.js
@@ -3,12 +3,16 @@ function getComputerChoice() {
     let randomNumber = Math.floor(Math.random() * 3) + 1;
     let computerChoice;
 
-    if (randomNumber === 1) {
-        computerChoice = "rock";
-    } else if (randomNumber === 2) {
-        computerChoice = "paper";
-    } else {
-        computerChoice = "scissors";
+    switch (randomNumber) {
+        case 1:
+            computerChoice = "rock";
+            break;
+        case 2:
+            computerChoice = "paper";
+            break;
+        case 3:
+            computerChoice = "scissors";
+            break;
     }
 
     return computerChoice;
@@ -19,41 +23,66 @@ function playOneRound(playerChoice, computerChoice) {
     let result;
     playerChoice = playerChoice.toLowerCase();
 
-    console.log(`You chose ${playerChoice}`);
-    console.log(`The computer chooses ${computerChoice}`);
+    console.log(`You chose ${playerChoice}, the computer chose ${computerChoice}`);
 
     if (playerChoice === computerChoice) {
-        result = "It is a tie! Play again!"
+        result = "It is a tie this round! Play again!"
         console.log(result);
         return result;
     } else if (playerChoice === "rock" && computerChoice === "paper") {
-        result = "You lose! Paper beats rock!";
+        result = "You lose the round! Paper beats rock!";
         console.log(result);
         return result;
     } else if (playerChoice === "rock" && computerChoice === "scissors") {
-        result = "You win! Rock beats scissors!";
+        result = "You win the round! Rock beats scissors!";
         console.log(result);
         return result;
     } else if (playerChoice === "paper" && computerChoice === "scissors") {
-        result = "You lose! Scissors beat paper!";
+        result = "You lose the round! Scissors beat paper!";
         console.log(result);
         return result;
     } else if (playerChoice === "paper" && computerChoice === "rock") {
-        result = "You win! Paper beats rock!";
+        result = "You win the round! Paper beats rock!";
         console.log(result);
         return result;
     } else if (playerChoice === "scissors" && computerChoice === "rock") {
-        result = "You lose! Rock beats scissors!";
+        result = "You lose the round! Rock beats scissors!";
         console.log(result);
         return result;
     } else {
-        result = "You win! Scissors beat paper!";
+        result = "You win the round! Scissors beat paper!";
         console.log(result);
         return result;
     }
 }
 
-playOneRound("scissors", getComputerChoice());
-playOneRound("rock", getComputerChoice());
-playOneRound("paper", getComputerChoice());
-playOneRound("paper", getComputerChoice());
+function game() {
+
+    let playerScore = 0;
+    let computerScore = 0;
+
+    for (let i = 1; i <= 5; i++) {
+        let result = playOneRound(prompt("Rock, Paper, or Scissors?", "Paper"), getComputerChoice());
+        if (result.includes("win")) {
+            playerScore++
+        } else if (result.includes("lose")) {
+            computerScore++
+        } else {
+            i--;
+        }
+    }
+
+    console.log("Player Score: " + playerScore);
+    console.log("Computer Score: " + computerScore);
+
+    if (playerScore > computerScore) {
+        console.log("Wou won the game!");
+    } else if (playerScore < computerScore) {
+        console.log("You lose the game!");
+    } else {
+        console.log("It was a tie this game! Play another game to win!");
+    }
+
+}
+
+game();
