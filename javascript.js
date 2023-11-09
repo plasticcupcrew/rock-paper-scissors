488c1c1c247d25ae35aa870c991d84902b3de55a
Michael Borrello <mborrello1@gmail.com>
2023-11-09 14:08:51 -0500

    Rework rps game to modify DOM elements
    Modify dom elements with event listeners on buttons instead of
    having the player see results in the console after writing
    out their choice in text.

--- javascript.js
+++ javascript.js
@@ -18,71 +18,64 @@ function getComputerChoice() {
     return computerChoice;
 }
 
-function playOneRound(playerChoice, computerChoice) {
+let player = 0;
+let computer = 0;
+const choices = document.querySelector(".choices");
+const result = document.querySelector(".result");
+const playerScore = document.querySelector(".player-score");
+const computerScore = document.querySelector(".computer-score");
 
-    let result;
+playerScore.textContent = "Player Score: 0";
+computerScore.textContent = "Computer Score: 0";
+
+function playOneRound(playerChoice, computerChoice) {
     playerChoice = playerChoice.toLowerCase();
 
-    console.log(`You chose ${playerChoice}, the computer chose ${computerChoice}`);
+    choices.textContent = 
+        `You chose ${playerChoice}, the computer chose ${computerChoice}`;
 
     if (playerChoice === computerChoice) {
-        result = "It is a tie this round! Play again!"
-        console.log(result);
-        return result;
+        result.textContent = "It is a tie this round! Play again!"
     } else if (playerChoice === "rock" && computerChoice === "paper") {
-        result = "You lose the round! Paper beats rock!";
-        console.log(result);
-        return result;
+        result.textContent = "You lose the round! Paper beats rock!";
+        computerScore.textContent = `Computer Score: ${++computer}`;
     } else if (playerChoice === "rock" && computerChoice === "scissors") {
-        result = "You win the round! Rock beats scissors!";
-        console.log(result);
-        return result;
+        result.textContent = "You win the round! Rock beats scissors!";
+        playerScore.textContent = `Player Score: ${++player}`;
     } else if (playerChoice === "paper" && computerChoice === "scissors") {
-        result = "You lose the round! Scissors beat paper!";
-        console.log(result);
-        return result;
+        result.textContent = "You lose the round! Scissors beat paper!";
+        computerScore.textContent = `Computer Score: ${++computer}`;
     } else if (playerChoice === "paper" && computerChoice === "rock") {
-        result = "You win the round! Paper beats rock!";
-        console.log(result);
-        return result;
+        result.textContent = "You win the round! Paper beats rock!";
+        playerScore.textContent = `Player Score: ${++player}`;
     } else if (playerChoice === "scissors" && computerChoice === "rock") {
-        result = "You lose the round! Rock beats scissors!";
-        console.log(result);
-        return result;
+        result.textContent = "You lose the round! Rock beats scissors!";
+        computerScore.textContent = `Computer Score: ${++computer}`;
     } else {
-        result = "You win the round! Scissors beat paper!";
-        console.log(result);
-        return result;
+        result.textContent = "You win the round! Scissors beat paper!";
+        playerScore.textContent = `Player Score: ${++player}`;
+    }
+
+    if(player === 5) {
+        result.textContent = `You won the game ${player} to ${computer}!`;
+        playerScore.textContent = "Player Score: 0";
+        computerScore.textContent = "Computer Score: 0";
+        player = 0;
+        computer = 0;
+    } else if (computer ===5) {
+        result.textContent = `You lose the game ${computer} to ${player}!`;
+        playerScore.textContent = "Player Score: 0";
+        computerScore.textContent = "Computer Score: 0";
+        player = 0;
+        computer = 0;
     }
 }
 
-function game() {
+let rockBtn = document.querySelector('#rock');
+rockBtn.addEventListener('click', () => playOneRound("rock", getComputerChoice()));
 
-    let playerScore = 0;
-    let computerScore = 0;
+let paperBtn = document.querySelector('#paper');
+paperBtn.addEventListener('click', () => playOneRound("paper", getComputerChoice()));
 
-    for (let i = 1; i <= 5; i++) {
-        let result = playOneRound(prompt("Rock, Paper, or Scissors?", "Paper"), getComputerChoice());
-        if (result.includes("win")) {
-            playerScore++
-        } else if (result.includes("lose")) {
-            computerScore++
-        } else {
-            i--;
-        }
-    }
-
-    console.log("Player Score: " + playerScore);
-    console.log("Computer Score: " + computerScore);
-
-    if (playerScore > computerScore) {
-        console.log("Wou won the game!");
-    } else if (playerScore < computerScore) {
-        console.log("You lose the game!");
-    } else {
-        console.log("It was a tie this game! Play another game to win!");
-    }
-
-}
-
-game();
+let scissorBtn = document.querySelector('#scissors');
+scissorBtn.addEventListener('click', () => playOneRound("scissors", getComputerChoice()));
